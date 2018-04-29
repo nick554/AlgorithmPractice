4beb36f801616bbe7a2a538ed24d44e38fcc422c
nick554 <xiaokong.chen@gmail.com>
2018-04-29 18:44:02 +0800

    daily update

--- LaiCode/LinkedList/MergeTwoLinkedList.java
+++ LaiCode/LinkedList/MergeTwoLinkedList.java
@@ -10,7 +10,7 @@ L1 = null, L2 = null, merge L1 and L2 to null
 
 /*
   Time:  O( n ) 
-  Space: O( 1 )
+  Space: recursion: O( n )  iteration: O( 1 )
 */
 
 /*
@@ -24,6 +24,7 @@ L1 = null, L2 = null, merge L1 and L2 to null
   }
 */
 
+// recursive solution
 public class Solution {
   // assumption: both list can be null/ have duplicate value
   public ListNode merge(ListNode one, ListNode two) {
@@ -38,5 +39,35 @@ public class Solution {
       two.next = merge(one, two.next);
       return two;
     }
+  }
+}
+
+// iterative solution
+public class Solution {
+  // assumption: both list can be null/ have duplicate value
+  public ListNode merge(ListNode one, ListNode two) {
+    // prehead to record the reference of the head
+    ListNode preHead = new ListNode(0);
+    ListNode current = preHead;
+    
+    while (one != null && two != null) {
+      if ( one.value <= two.value ) {
+        current.next = one;
+        current = one;
+        one = one.next;
+      } else {
+        current.next = two;
+        current = two;
+        two = two.next;
+      }
+    }
+    
+    if (one == null) {
+      current.next = two;
+    } else {
+      current.next = one;
+    }
+    
+    return preHead.next;
   }
 }
